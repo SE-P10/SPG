f44b0b658740445b42ad70d532a484dc514b0045
Michele Basilico <s282513@studenti.polito.it>
2021-11-07 10:59:32 +0100

    Doc: Added test environment configuration(Jest+React Testing Library) + First Test on App.js

---
+++ client/src/App.test.js
@@ -0,0 +1,9 @@
+import App from "./App"
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Smoke Test -> The “smoke test” checks that a component renders without throwing (This test renders App with its children)
+it('App renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
