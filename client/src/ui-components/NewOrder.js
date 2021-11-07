1d95370aec264d31aa717af6697e3d0c4100a38f
AmedeoSarpa <amedeosarpa@tiscali.it>
2021-11-07 18:34:30 +0100

    add browse product

--- client/src/ui-components/NewOrder.js
+++ client/src/ui-components/NewOrder.js
@@ -1,18 +1,38 @@
-import { Button, Alert, Form, Row } from "react-bootstrap";
+import { Button, Alert, Form, Row, Col } from "react-bootstrap";
 import { useState } from "react";
 import { useEffect } from "react";
 
 function NewOrder(props) {
+
+  const handleSubmit = (event,props) => {
+
+
+
+    props.changeAction(0);
+
+  }
+
+
+
   const [products, setProducts] = useState([
     { id: 1, quantity: 2, price: 20.5, name: "test" },
-    { id: 1, quantity: 2, price: 20.5, name: "test" },
-    { id: 1, quantity: 2, price: 20.5, name: "test" },
+    { id: 2, quantity: 2, price: 20.5, name: "test" },
+    { id: 3, quantity: 2, price: 20.5, name: "test" },
   ]);
-  const [orderProduct, setOrderProducts] = useState([]);
+  
+  const [orderProduct, setOrderProducts] = useState([{idProduct : 1 , quantity : 0 },{idProduct : 2 , quantity : 0 },{idProduct : 3 , quantity : 0 }]);
+  const [users,setUsers] = useState({email:"user1"},{email:"user2"})
+
   useEffect(() => {
     const fillTables = async () => {
       //const productsTmp = await API.getProducts();
+      //const userTmp = await API.getUsers();
       //setProducts(productsTmp);
+      //setUsers(usersTmp);
+      //for (let p of productsTmp ){
+      //  setOrderProducts(oldList => {return oldList.concat({idProduct : p.id,quantity : 0})})
+      //}
+      
     };
 
     fillTables();
@@ -20,10 +40,27 @@ function NewOrder(props) {
   //return <> NewOrder </>;
 
   return (
-    <>
+    <> <Form>
       {products.map((p) => (
-        <>{p.id}</>
+        <Row>
+        {p.id}{" "}
+        {p.name}{" "}
+        {p.price}{" "} 
+        <Col><Form.Check   /></Col>
+
+        
+        </Row>
+
+        
+
       ))}
+
+<Button
+                className='se-button btn-block'
+                onClick={(ev) => handleSubmit(ev,props)}>
+                Register a Client
+          </Button>
+      </Form>
     </>
   );
 }
