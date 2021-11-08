63c64d950dbeb99af7bbcaa83f11d76907397a62
Michele Basilico <s282513@studenti.polito.it>
2021-11-08 16:49:58 +0100

    Doc: Tests on Shop Employee

--- client/src/pages/Login.test.js
+++ client/src/pages/Login.test.js
@@ -2,6 +2,16 @@ import LoginForm from "./Login"
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 //import '@testing-library/jest-dom'; in setupTests.js
+import ReactDOM from 'react-dom';
+
+//JEST
+//Smoke Test -> The “smoke test” checks that a component renders without throwing (This test renders App with its children)
+it('LoginForm renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<LoginForm />, div);
+  });
+
+
 
 //React Testing Library(to test components in isolation from the child components they render)
 it('Login renders username text', () => {
---
+++ client/src/pages/ShopEmployee.test.js
@@ -0,0 +1,28 @@
+import LoginForm from "./Login"
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+//import '@testing-library/jest-dom'; in setupTests.js
+import ReactDOM from 'react-dom';
+import { ShopEmployee } from "./ShopEmployee";
+
+//JEST
+//Smoke Test -> The “smoke test” checks that a component renders without throwing (This test renders App with its children)
+it('ShopEmployee renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<ShopEmployee />, div);
+  });
+
+
+
+//React Testing Library(to test components in isolation from the child components they render)
+it('ShopEmployee renders buttons', () => {
+    render(<ShopEmployee />);
+    expect(screen.getByText('ShopEmployee personal page')).toBeInTheDocument();
+    expect(screen.getByText('Register a Client')).toBeInTheDocument();
+    expect(screen.getByText('Browse Products')).toBeInTheDocument();
+    expect(screen.getByText('TopUp a Wallet')).toBeInTheDocument();
+    expect(screen.getByText('New Order')).toBeInTheDocument();
+    expect(screen.getByText('HandOut')).toBeInTheDocument();
+    expect(screen.getByText('Check Order')).toBeInTheDocument();
+
+});
