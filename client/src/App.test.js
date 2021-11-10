b2db0f38527b42b5e8de524431cec89c000ef441
Michele Basilico <s282513@studenti.polito.it>
2021-11-10 15:35:55 +0100

    Doc: Tests on Shop Employee

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -9,3 +9,8 @@ it('App renders without crashing', () => {
     ReactDOM.render(<App />, div);
   });
 
+// Altrà possibilità e quella di testare i componenti attraverso gli snapShot,la loro comodità riguarda il fatto che il componente viene fotografato
+// nell'istante in cui viene eseguito il primo test, dopodiche ogni suo cambiamento verrà considerato come un test fallito.
+// Questo permette di seguire le modifiche, se sono voloute, è possibile aggiornare lo snapshot
+// E' possibile anche testare snapshot multipli(ricordando di aggiornare il precedente snapshot che diventa non più funzionale),
+// questo permette di testare ad esempio passando delle props differenti
