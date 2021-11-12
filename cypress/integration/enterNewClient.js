2ec0baac7c771418031ab807a1feb5712a4a2885
BMike10 <s282513@studenti.polito.it>
2021-11-12 19:17:09 +0100

    Doc: Added the enterNewClient Test prototype

---
+++ cypress/integration/enterNewClient.js
@@ -0,0 +1,11 @@
+describe('enterNewClient', () => {
+    it('a shopEmployee should be able to add a new Client ', () => {
+        //Go to Login Page
+        //Login as a ShopEmployee
+        //Click a button to add new Client
+        //Insert the Client Info
+        //Click add button
+        //Return to Clients List
+        //Find the client added
+    })
+})
