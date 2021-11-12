537f5beb3a2881cfa03306d1cabd7daf4f250506
Alessandro Morelli <amorelli96@gmail.com>
2021-11-12 11:57:21 +0100

    API.js function

--- client/src/API.js
+++ client/src/API.js
@@ -66,12 +66,31 @@ async function getUserInfo(userID) {
   }
 }
 
+async function updateWallet(amount, client_email) {
+  const response = await ("/api/wallet/update", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      amount: amount,
+      client_email: client_email,
+    }),
+  });
+  if (response.ok) {
+    return await response.json();
+  } else {
+    throw await response.json();
+  }
+}
+
 
 const API = {
   ...gApi,
   logIn,
   logOut,
-  getUserInfo
+  getUserInfo,
+  updateWallet
 };
 
 export default API;
