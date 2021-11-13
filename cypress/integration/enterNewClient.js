6e3053d623a26f2e1227bc93c7f78999bfbeb1ad
BMike10 <s282513@studenti.polito.it>
2021-11-13 11:49:59 +0100

    Doc: Updated viewProducts Test; New tests about enterNewClient

--- cypress/integration/enterNewClient.js
+++ cypress/integration/enterNewClient.js
@@ -1,11 +1,82 @@
 describe('enterNewClient', () => {
-    it('a shopEmployee should be able to add a new Client ', () => {
+    it('a shopEmployee should be able to add a new Client (by entering correct info) ', () => {
         //Go to Login Page
+        cy.visit('http://localhost:3000');
+        cy.findByRole('link', { name: /login/i }).click();
         //Login as a ShopEmployee
+        cy.findByRole('textbox', { name: /username/i }).type('john.doe@demo01.it');
+        cy.findByLabelText(/password/i).type('password');
+        cy.findByRole('button', { name: /login/i }).click();
         //Click a button to add new Client
+        cy.findByRole('button', { name: /register a client/i }).click();
         //Insert the Client Info
-        //Click add button
-        //Return to Clients List
-        //Find the client added
+        cy.get('#formGridName').type('Michele')
+        cy.get('#formGridSurname').type('Basilico')
+        cy.get('#formGridUsername').type('Miki')
+        cy.get('#formGridEmail').type('michele@gmail.com')
+        cy.get('#formGridPassword').type('ciao')
+        cy.get('#formGridConfirmPassword').type('ciao')
+        //Click register button
+        cy.get('.spg-button').click()
+        //Succesfully Assert is shown
+        cy.findByRole('alert').should('have.text', '×Close alert successfully registered customer ')
+        //Find the client added?
+    })
+    it('a shopEmployee should be able to add a new Client (by entering wrong password couple) ', () => {
+        //Go to Login Page
+        cy.visit('http://localhost:3000');
+        cy.findByRole('link', { name: /login/i }).click();
+        //Login as a ShopEmployee
+        cy.findByRole('textbox', { name: /username/i }).type('john.doe@demo01.it');
+        cy.findByLabelText(/password/i).type('password');
+        cy.findByRole('button', { name: /login/i }).click();
+        //Click a button to add new Client
+        cy.findByRole('button', { name: /register a client/i }).click();
+        //Insert the Client Info
+        cy.get('#formGridName').type('Michele')
+        cy.get('#formGridSurname').type('Basilico')
+        cy.get('#formGridUsername').type('Miki')
+        cy.get('#formGridEmail').type('michele@gmail.com')
+        cy.get('#formGridPassword').type('ciao')
+        cy.get('#formGridConfirmPassword').type('ciao2')
+        //Click register button
+        cy.get('.spg-button').click()
+        //Succesfully Assert is shown
+        cy.findByRole('alert').should('have.text', ' Password Mismatch ')
+    })
+
+    it('a shopEmployee should be able to add a new Client (by missing info) ', () => {
+        //Go to Login Page
+        cy.visit('http://localhost:3000');
+        cy.findByRole('link', { name: /login/i }).click();
+        //Login as a ShopEmployee
+        cy.findByRole('textbox', { name: /username/i }).type('john.doe@demo01.it');
+        cy.findByLabelText(/password/i).type('password');
+        cy.findByRole('button', { name: /login/i }).click();
+        //Click a button to add new Client
+        cy.findByRole('button', { name: /register a client/i }).click();
+        //Insert the Client Info without some info
+        //case0 -> Nessuna info
+        cy.get('.spg-button').click()
+        cy.findByRole('alert').should('have.text', ' Missing Data, check all the fields ')
+        //case1 -> Una info mancante
+        cy.get('#formGridName').type('Michele')
+        cy.get('#formGridUsername').type('Miki')
+        cy.get('#formGridEmail').type('michele@gmail.com')
+        cy.get('#formGridPassword').type('ciao')
+        cy.get('#formGridConfirmPassword').type('ciao')
+        cy.get('.spg-button').click()
+        cy.findByRole('alert').should('have.text', ' Missing Data, check all the fields ')
+        //case2 -> Una info presente
+        cy.get('#formGridName').clear()
+        cy.get('#formGridUsername').clear()
+        cy.get('#formGridEmail').clear()
+        cy.get('#formGridPassword').clear()
+        cy.get('#formGridConfirmPassword').clear()
+        cy.get('#formGridSurname').type('Basilico')
+        //Click register button
+        cy.get('.spg-button').click()
+        //Succesfully Assert is shown
+        cy.findByRole('alert').should('have.text', ' Missing Data, check all the fields ')
     })
 })
--- cypress/integration/viewProducts.js
+++ cypress/integration/viewProducts.js
@@ -5,11 +5,27 @@ describe('viewProducts', () => {
         cy.findByRole('link', {  name: /login/i}).click();
         //Login
         cy.findByRole('textbox', {  name: /username/i}).type('john.doe@demo01.it');
-        cy.findByLabelText(/password/i).type('$2b$10$OMHdOZ.PATpbMoaDz5013edi5QCTEFgpRv7Vn8OyDHQNN/4KXUKdi');
+        cy.findByLabelText(/password/i).type('password');
         cy.findByRole('button', {  name: /login/i}).click();
-        //User views a products list
-        
-        //User chooses a product in the list
+        //Click on Browse Products
+        cy.findByRole('button', {  name: /browse products/i}).click();
+        //User views a products list  -> TODO: Redo using a for cycle and prebuilded data
+        cy.get(':nth-child(1) > h4').should('have.text', ' Name ')
+        cy.get(':nth-child(3) > .container > :nth-child(3) > :nth-child(1)').should('have.text', ' banana')
+        cy.get(':nth-child(4) > :nth-child(1)').should('have.text', ' apple')
+        cy.get(':nth-child(5) > :nth-child(1)').should('have.text', ' melon')
+
+        cy.get(':nth-child(2) > h4').should('have.text', ' Quantity')
+        cy.get(':nth-child(3) > :nth-child(2)').should('have.text', ' 3')
+        cy.get(':nth-child(4) > :nth-child(2)').should('have.text', ' 4')
+        cy.get(':nth-child(5) > :nth-child(2)').should('have.text', ' 3')
+
+        cy.get(':nth-child(3) > h4').should('have.text', ' Price ')
+        cy.get(':nth-child(3) > :nth-child(3)').should('have.text', '2.4')
+        cy.get(':nth-child(4) > :nth-child(3)').should('have.text', '2.3')
+        cy.get(':nth-child(5) > :nth-child(3)').should('have.text', '2.4')
+
+        //User chooses a product in the list?
 
     })
 })
