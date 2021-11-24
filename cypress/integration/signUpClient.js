39e360fe4f0873a78c19c1d03e31ac5a545b5503
BMike10 <s282513@studenti.polito.it>
2021-11-24 15:53:55 +0100

    Doc: EndToEnd Test started (signUpClient); New api is added to clear the db

---
+++ cypress/integration/signUpClient.js
@@ -0,0 +1,218 @@
+describe('signUp_Client', () => {
+
+    before(() => {
+        //runs once before all tests in the block -> Add new Client
+        //Go to Login Page
+        //cy.visit('http://localhost:3000');
+        //cy.findByRole('link', { name: /Sign up  /i }).click();
+
+    })
+
+    beforeEach(() => {
+        // runs before each test in the block
+        //Go to Login Page
+        cy.visit('http://localhost:3000');
+        cy.findByRole('link', { name: /Sign up/i }).click();
+
+    })
+
+    afterEach(() => {
+
+    })
+
+    after(() => {
+
+        //clear Db
+        //cy.request('DELETE', 'http://localhost:3001/api/clients/michelebasilico@gmail.com')
+        //cy.request('DELETE', 'http://localhost:3001/api/clients/michele@gmail.com')
+
+    })
+
+
+    it('a new client should fill each field before to sign up  ', () => {
+
+        //Insert the Client Info without name
+        //cy.findByRole('formGridName').contains().type('Michele')
+        cy.get('#formGridSurname').type('Basilico')
+        cy.get('#formGridUsername').type('Miki')
+        cy.get('#formGridEmail').type('michele@gmail.com')
+        cy.get('#formGridPassword').type('ciao')
+        cy.get('#formGridConfirmPassword').type('ciao')
+        //Click register button
+        cy.get('.spg-button').click()
+        //ALert is shown
+        cy.findByRole('alert').should('include.text', 'Missing Data, check all the fields')
+        //Close Alert
+        cy.findByText(/×/i).click()
+        //check on the server
+
+        //Insert the Client Info without surname
+        //cy.findByRole('formGridName').contains().type('Michele')
+        cy.get('#formGridName').type('Michele')
+        cy.get('#formGridSurname').clear()
+        //Click register button
+        cy.get('.spg-button').click()
+        //ALert is shown
+        cy.findByRole('alert').should('include.text', 'Missing Data, check all the fields')
+        //Close Alert
+        cy.findByText(/×/i).click()
+        //check on the server
+
+
+        //Insert the Client Info without username
+        //cy.findByRole('formGridName').contains().type('Michele')
+        cy.get('#formGridSurname').type('Basilico')
+        cy.get('#formGridUsername').clear()
+        //Click register button
+        cy.get('.spg-button').click()
+        //ALert is shown
+        cy.findByRole('alert').should('include.text', 'Missing Data, check all the fields')
+        //Close Alert
+        cy.findByText(/×/i).click()
+        //check on the server
+
+        //Insert the Client Info without password
+        //cy.findByRole('formGridName').contains().type('Michele')
+        cy.get('#formGridUsername').type('Miki')
+        cy.get('#formGridPassword').clear()
+        //Click register button
+        cy.get('.spg-button').click()
+        //ALert is shown
+        cy.findByRole('alert').should('include.text', 'Missing Data, check all the fields')
+        //Close Alert
+        cy.findByText(/×/i).click()
+        //check on the server
+
+        //Insert the Client Info without confirm password
+        //cy.findByRole('formGridName').contains().type('Michele')
+        cy.get('#formGridPassword').type('Miki')
+        cy.get('#formGridConfirmPassword').clear()
+        //Click register button
+        cy.get('.spg-button').click()
+        //ALert is shown
+        cy.findByRole('alert').should('include.text', 'Missing Data, check all the fields')
+        //Close Alert
+        cy.findByText(/×/i).click()
+        //check on the server
+
+        //Insert the Client Info without password and its confirm
+        //cy.findByRole('formGridName').contains().type('Michele')
+        cy.get('#formGridPassword').clear()
+        //Click register button
+        cy.get('.spg-button').click()
+        //ALert is shown
+        cy.findByRole('alert').should('include.text', 'Missing Data, check all the fields')
+        //Close Alert
+        cy.findByText(/×/i).click()
+        //check on the server
+
+        //Insert the Client Info without email
+        //cy.findByRole('formGridName').contains().type('Michele')
+        cy.get('#formGridPassword').type('ciao')
+        cy.get('#formGridConfirmPassword').type('ciao')
+        cy.get('#formGridEmail').clear()
+
+        //Click register button
+        cy.get('.spg-button').click()
+        //ALert is shown
+        cy.findByRole('alert').should('include.text', 'Missing Data, check all the fields')
+        //Close Alert
+        cy.findByText(/×/i).click()
+        //check on the server
+
+        //Insert the Client Info without username,name, surname and email
+        //cy.findByRole('formGridName').contains().type('Michele')
+        cy.get('#formGridName').clear()
+        cy.get('#formGridSurname').clear()
+        cy.get('#formGridEmail').clear()
+        cy.get('#formGridUsername').clear()
+
+
+        //Click register button
+        cy.get('.spg-button').click()
+        //ALert is shown
+        cy.findByRole('alert').should('include.text', 'Missing Data, check all the fields')
+        //Close Alert
+        cy.findByText(/×/i).click()
+        //check on the server
+
+        //Insert no data
+        //cy.findByRole('formGridName').contains().type('Michele')
+        cy.get('#formGridName').clear()
+        cy.get('#formGridSurname').clear()
+        cy.get('#formGridEmail').clear()
+        cy.get('#formGridPassword').clear()
+        cy.get('#formGridConfirmPassword').clear()
+        cy.get('#formGridUsername').clear()
+
+        //Click register button
+        cy.get('.spg-button').click()
+        //ALert is shown
+        cy.findByRole('alert').should('include.text', 'Missing Data, check all the fields')
+        //Close Alert
+        cy.findByText(/×/i).click()
+        //check on the server
+
+
+    })
+
+    it('a new client should not be able to sign up entering two different password  ', () => {
+
+        //Insert the Client Info without name
+        cy.get('#formGridName').type('Michele')
+        cy.get('#formGridSurname').type('Basilico')
+        cy.get('#formGridUsername').type('Miki')
+        cy.get('#formGridEmail').type('michele@gmail.com')
+        cy.get('#formGridPassword').type('ciao')
+        cy.get('#formGridConfirmPassword').type('ciao1')
+        //Click register button
+        cy.get('.spg-button').click()
+        //ALert is shown
+        cy.findByRole('alert').should('include.text', 'Password Mismatch')
+        //Close Alert
+        cy.findByText(/×/i).click()
+        //check on the server
+    })
+    /*
+        it('a new client should not be able to sign up entering a password that does not respect estabilished criterian  ', () => {
+    
+            //Insert the Client Info without name
+            cy.findByRole('formGridName').contains().type('Michele')
+            cy.get('#formGridSurname').type('Basilico')
+            cy.get('#formGridUsername').type('Miki')
+            cy.get('#formGridEmail').type('michele@gmail.com')
+            cy.get('#formGridPassword').type('ciao')
+            cy.get('#formGridConfirmPassword').type('ciao1')
+            //Click register button
+            cy.get('.spg-button').click()
+            //ALert is shown
+            cy.findByRole('alert').should('include.text', 'Password Mismatch')
+            //Close Alert
+            cy.findByText(/×/i).click()
+            //check on the server
+        })
+        */
+    it('a new client should be able to sign up entering correct info', () => {
+
+        //Insert the Client Info without name
+        cy.get('#formGridName').contains().type('Michele')
+        cy.get('#formGridSurname').type('Basilico')
+        cy.get('#formGridUsername').type('Miki')
+        cy.get('#formGridEmail').type('michele@gmail.com')
+        cy.get('#formGridPassword').type('ciao')
+        cy.get('#formGridConfirmPassword').type('ciao')
+        //Click register button
+        cy.get('.spg-button').click()
+        //ALert is shown
+        cy.findByRole('alert').should('include.text', 'New client registered')
+        //Close Alert
+        cy.findByText(/×/i).click()
+        //check on the server
+
+        //Logout
+        cy.get('#navbarScrollingDropdown').click()
+        cy.get('.dropdown-menu > :nth-child(2)').click()
+        cy.clearCookies()
+    })
+
+})
