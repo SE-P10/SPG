c3f11176009a446e3f305107e804dca4aeaa3b81
sh1zen <dev.sh1zen@outlook.it>
2021-12-02 15:10:57 +0100

    Added getRequestedProducts API

--- client/src/api/orders.js
+++ client/src/api/orders.js
@@ -1,20 +1,4 @@
-/**
- * Handle server communication
- *
- * @param {*} filter
- * @param {*} products
- * @param {*} order_details
- * @param {*} method
- * @returns miscellaneous
- */
-async function handleOrderAction(
-  filter,
-  products = [],
-  order_details = {},
-  method = "POST"
-) {
-  if (typeof order_details === "number") order_details = { id: order_details };
-  else order_details = Object.assign({ id: 0 }, order_details);
+async function handleFetch(endpoint, body = {}, method = "POST") {
 
   let request;
 
@@ -35,12 +19,12 @@ async function handleOrderAction(
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ products: products, order: order_details }),
+        body: JSON.stringify(body)
       };
   }
 
   return new Promise((resolve, reject) => {
-    fetch("/api/orders/" + filter, request)
+    fetch(endpoint, request)
       .then(async (response) => {
         try {
           let parsed = await response.json();
@@ -59,6 +43,35 @@ async function handleOrderAction(
   });
 }
 
+
+/**
+ * Handle server communication
+ *
+ * @param {Number | String} filter
+ * @param {Array} products
+ * @param {Object} order_details
+ * @param {String} method
+ * @returns {*}
+ */
+async function handleOrderAction(filter, products = [], order_details = {}, method = "POST") {
+
+  if (typeof order_details === "number") {
+    order_details = { id: order_details };
+  }
+  else {
+    order_details = Object.assign({ id: 0 }, order_details);
+  }
+
+  return handleFetch("/api/orders/" + filter, { products: products, order: order_details }, method)
+}
+
+/**
+ * 
+ * @param {*} response 
+ * @param {String} type 
+ * @param {Boolean} falseRes 
+ * @returns {*}
+ */
 async function parseResponse(response, type = "boolnum", falseRes = false) {
   response = await response;
 
@@ -80,8 +93,8 @@ async function parseResponse(response, type = "boolnum", falseRes = false) {
 
 /**
  *
- * @param {*} filter
- * @returns { id: 0, user_id: 0, status: '', price: 0, pickup_time: '', pickup_place: '', 'user':{id: 0, username: '', email: '', name: '', surname: ''}, 'products': [{order_id: 0,product_id: '', quantity: 0}]}
+ * @param {Number | String} filter user_email|orderID|order_status
+ * @returns {Object} { id: 0, user_id: 0, status: '', price: 0, pickup_time: '', pickup_place: '', 'user':{id: 0, username: '', email: '', name: '', surname: ''}, 'products': [{order_id: 0,product_id: '', quantity: 0}]}
  */
 async function getOrders(filter) {
   return parseResponse(
@@ -93,9 +106,8 @@ async function getOrders(filter) {
 
 /**
  * Frontend interface API
- *
- * @param {*} filter
- * @returns order in pending status
+ * 
+ * @returns {Array} orders in pending status
  */
 async function getPendingOrders() {
   return getOrders("pending");
@@ -104,8 +116,8 @@ async function getPendingOrders() {
 /**
  * Frontend interface API
  *
- * @param {*} orderID
- * @returns true|false
+ * @param {Number} orderID
+ * @returns {boolean} true|false
  */
 async function handOutOrder(orderID = 0) {
   return parseResponse(
@@ -120,10 +132,10 @@ async function handOutOrder(orderID = 0) {
 /**
  * Frontend interface API
  *
- * @param {*} userID
- * @param {*} products
- * @param {*} order_details
- * @returns true|false
+ * @param {number} userID
+ * @param {Array} products
+ * @param {Object} order_details
+ * @returns {boolean} true|false
  */
 async function insertOrder(userID, products = [], order_details = {}) {
   return parseResponse(
@@ -131,11 +143,23 @@ async function insertOrder(userID, products = [], order_details = {}) {
   );
 }
 
+/**
+ * Frontend interface API
+ *
+ * @param {Number} farmerID
+ * @returns {Array} [id: 2, quantity: 5, name: 'apple']
+ */
+async function getRequestedProducts(farmerID) {
+  return parseResponse(
+    await handleFetch("/api/orders/products/farmer/" + farmerID, {}, "GET") , 'array');
+}
+
 const ordersApi = {
   insertOrder,
   handOutOrder,
   getPendingOrders,
   getOrders,
+  getRequestedProducts
 };
 
 export default ordersApi;
