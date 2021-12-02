5323f9b185f5c640410f7fe5e5f845e6ad49f7d3
sh1zen <dev.sh1zen@outlook.it>
2021-12-02 17:38:19 +0100

    update

--- server/dao/notification-dao.js
+++ server/dao/notification-dao.js
@@ -6,7 +6,36 @@ const AF_DEBUG_PROCESS = AF_DEBUG;
 
 const db = require("../db");
 const { getUserById } = require("./user-dao");
-const { debugLog, setNotification } = require("../utility");
+const { debugLog } = require("../utility");
+
+
+async function addNotification(userID, data, email = false) {
+
+    let sql = 'INSERT INTO notification (user_id, data) VALUES(?, ?)';
+
+    let status = await this.runQuerySQL(db, sql, [userID, data], true);
+
+    if (status && email) {
+
+        email = this.filter_args({
+            to: '',
+            body: data,
+            subject: ''
+        }, email);
+
+        return this.sendMail(email.to, email.body, email.subject);
+    }
+
+    return status;
+}
+
+
+async function setNotification(notificationID) {
+
+    let sql = 'UPDATE notification SET seen = 1 WHERE id = ?';
+
+    return this.runQuerySQL(db, sql, [notificationID], true);
+}
 
 
 exports.execApi = (app, passport, isLoggedIn) => {
@@ -23,7 +52,25 @@ exports.execApi = (app, passport, isLoggedIn) => {
         }
 
         try {
-            let status = await setNotification(db, user.id, req.body.text, { to: user.email });
+            let status = await addNotification(user.id, req.body.text, { to: user.email });
+
+            if (status)
+                res.status(201).json(status).end();
+            else
+                res.status(400).json({ error: 'Unable to insert user notification' });
+
+        } catch (err) {
+            debugLog(err)
+            res.status(503).json({ error: err });
+        }
+
+    });
+
+    // insert a new notification /api/notification/:user_id
+    app.put('/api/notification/:id', AF_ALLOW_DIRTY ? (req, res, next) => { return next() } : isLoggedIn, async (req, res) => {
+
+        try {
+            let status = await setNotification(req.params.id);
 
             if (status)
                 res.status(201).json(status).end();
